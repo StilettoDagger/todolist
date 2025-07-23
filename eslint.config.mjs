ee1bcb17e1e4f5ae7d00c0ce8a5c5fc157afa7c5
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2025-07-23 17:15:48 +0300

    add dist folder to be ignored by eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import json from "@eslint/json";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig([
@@ -21,4 +21,5 @@ export default defineConfig([
 		extends: ["json/recommended"],
 	},
 	eslintConfigPrettier,
+	globalIgnores(["dist/"]),
 ]);
